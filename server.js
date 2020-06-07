dc96ac39468ec9fc0c235ec3383695357256a882
Rebekka Haley <rebekka.haley@gmail.com>
2020-06-07 14:21:36 +0100

    [udacity] lesson 4 / add travis yml and env vars in app code

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+    // start of updated code
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
+    // end of updated code
+
     console.log("Microservices rock!");
     await sleep(5000);
   }
