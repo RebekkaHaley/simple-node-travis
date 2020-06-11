6bc52c0a6d2df382707b81e6ef5828cc561cf7bf
Rebekka Haley <rebekka.haley@gmail.com>
2020-06-11 23:08:05 +0100

    [udacity] lesson 5 / getting env variables to work

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// set local variable populated by environment variable $FAVORITE_FOOD
-const favoriteFood = process.env.FAVORITE_FOOD;
+// set local variable populated by environment variable $FAVOURITE_FOOD
+const FAVOURITE_FOOD = process.env.FAVOURITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,7 +9,7 @@ async function main() {
   while(true) {
     
     // start of updated code
-    console.log(`My favorite food is ${favoriteFood}`);
+    console.log(`My favourite food is ${FAVOURITE_FOOD} <3`);
     // end of updated code
 
     console.log("Microservices rock!");
